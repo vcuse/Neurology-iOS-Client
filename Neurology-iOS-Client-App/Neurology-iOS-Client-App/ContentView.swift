a26a953fbc1f7ff918bad0a701b92b84ffdee9b2
Lauren Viado <lauren.viado1@gmail.com>
2024-07-24 16:04:23 -0400

    xcodeprojFiles

---
+++ Neurology-iOS-Client-App/Neurology-iOS-Client-App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Neurology-iOS-Client-App
+//
+//  Created by Lauren Viado on 7/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Neurology-iOS-Client-App/Neurology-iOS-Client-App/Neurology_iOS_Client_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Neurology_iOS_Client_AppApp.swift
+//  Neurology-iOS-Client-App
+//
+//  Created by Lauren Viado on 7/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Neurology_iOS_Client_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
