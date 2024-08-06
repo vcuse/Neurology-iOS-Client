06b7c1d491c0e4e95261b1f4b1d07a9c419fb18f
Lauren Viado <lauren.viado1@gmail.com>
2024-08-06 12:13:26 -0400

    sign in view

---
+++ Neurology-App-UI/Neurology-iOS-Client-App-Tester/Neurology-iOS-Client-App-Tester/SignInView.swift
@@ -0,0 +1,109 @@
+//
+//  SignInView.swift
+//  Neurology-iOS-Client-App
+//
+//  Created by Lauren Viado on 8/6/24.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
+    @State private var isPasswordVisible: Bool = false
+    
+    var body: some View {
+        GeometryReader { geometry in
+            VStack (spacing: 30) {
+                Text("Hello, user!")
+                    .font(.title)
+                    .bold()
+                    .padding(30)
+                
+                VStack (alignment: .leading) {
+                    Text("Username")
+                        .font(.headline)
+                        .padding(.top)
+                        .padding(.leading)
+                        .padding(.trailing)
+                    
+                    
+                    
+                    TextField("Enter your username", text: $username)
+                        .padding()
+                        .background(Color(.systemGray6))
+                        .cornerRadius(5)
+                        .autocapitalization(/*@START_MENU_TOKEN@*/.none/*@END_MENU_TOKEN@*/)
+                        .disableAutocorrection(true)
+                        .padding(.leading)
+                        .padding(.trailing)
+                        .padding(.bottom)
+                    
+                    HStack {
+                        Text("Password")
+                            .font(.headline)
+                            .padding(.top)
+                            .padding(.leading)
+                            .padding(.trailing)
+                        
+                        Button(action: {
+                            isPasswordVisible.toggle()
+                        }) {
+                            Image(systemName: isPasswordVisible ? "eye.fill" : "eye.slash.fill")
+                                .foregroundColor(.gray)
+                        }
+                        .padding(.bottom, -13)
+                        .padding(.leading, -5)
+                    }
+                    
+                    HStack {
+                        if isPasswordVisible {
+                            TextField("Enter your password", text: $password)
+                                .padding()
+                                .background(Color(.systemGray6))
+                                .cornerRadius(5)
+                                .autocapitalization(/*@START_MENU_TOKEN@*/.none/*@END_MENU_TOKEN@*/)
+                                .disableAutocorrection(true)
+                                .padding(.leading)
+                                .padding(.trailing)
+                                .padding(.bottom)
+                        } else {
+                            SecureField("Enter your password", text: $password)
+                                .padding()
+                                .background(Color(.systemGray6))
+                                .cornerRadius(5)
+                                .autocapitalization(/*@START_MENU_TOKEN@*/.none/*@END_MENU_TOKEN@*/)
+                                .disableAutocorrection(true)
+                                .padding(.leading)
+                                .padding(.trailing)
+                                .padding(.bottom)
+                        }
+                        
+                    }
+                    
+                }
+                
+                
+                Button(action: {
+                    // add sign in functionality here
+                }) {
+                    Text("Sign In")
+                        .foregroundColor(.black)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.yellow)
+                        .cornerRadius(20)
+                }
+                .padding(20)
+            }
+            
+        }
+        
+        
+    }
+    
+}
+
+#Preview {
+    SignInView()
+}
