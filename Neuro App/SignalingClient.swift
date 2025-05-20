38a62d13a0c64185f11de806b2694159e5b046bd
David <ferrufinoda2@vcu.edu>
2025-05-20 16:00:49 -0400

    fixed an issue with the builds not setting the urls

--- Neuro App/SignalingClient.swift
+++ Neuro App/SignalingClient.swift
@@ -128,11 +128,11 @@ final class SignalingClient: NSObject, RTCPeerConnectionDelegate, ObservableObje
     func getAddress(url: URL) {
 
         // var uniqueID = ""
-        let options = PeerJSOption(host: "videochat-signaling-app.ue.r.appspot.com",
-                                   port: 443,
+        let options = PeerJSOption(host: AppURLs.hostURL,
+                                   port: AppURLs.portNumber,
                                    path: "/",
                                    key: "your_key_here",
-                                   secure: true)
+                                   secure: AppURLs.secure)
 
         let api = API(options: options, url: url)
         print(api.self)
